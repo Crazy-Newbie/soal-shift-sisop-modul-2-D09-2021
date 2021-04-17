a38a52a8919c4fc5687d16e25f03b349871c8990
Crazy-Newbie <55473294+Crazy-Newbie@users.noreply.github.com>
2021-04-17 22:09:09 +0800

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -9,14 +9,49 @@
 char location[] = "/home/ezhie/modul2/petshop";
 int status;
 struct dirent *dp;
-DIR *dir = opendir(basePath);
-FILE *fptr;
-if (!dir)
-    return;
+
+
+void listFilesRecursively(char *Path){
+  char path[1000];
+  pid_t pid3;
+  DIR *dir = opendir(Path);
+  struct dirent *dp;
+ 
+  if(!dir) return;
+ 
+  while ((dp = readdir(dir)) != NULL){
+	if(strcmp(dp->d_name,".") != 0 && strcmp(dp->d_name,"..") != 0){
+    
+  	//2b
+  	char token[100]= "";
+  	strcpy(token,dp->d_name);
+  	char pfolder[100] = "/home/ezhie/modul2/petshop/";
+  	strtok(token,";");
+  	strcat(pfolder,token);
+  	pid3 = fork();
+  	if(pid3 == 0){
+    	char *argv3[] = {"mkdir","-p",pfolder,NULL};
+    	execv("/usr/bin/mkdir",argv3);
+  	}
+  	else{
+    	((wait(&status))>0);
+  	}
+ 	 
+  	strcpy(path,Path);
+  	strcpy(path,"/");
+  	strcat(path,dp->d_name);
+ 	 
+  	listFilesRecursively(path);
+	}
+  }	 
+ 
+  closedir(dir);  
+} 	 
+
 
 int main()
 {
-    pid_t pid1,pid2,pid3,pid4;
+    pid_t pid1,pid2;
 
     if(pid1 < 0){
        return EXIT_FAILURE;
@@ -37,25 +72,12 @@ int main()
             char *argv[] = {"unzip", "pets.zip", "*.jpg", "-d", location, NULL};
             execv("/usr/bin/unzip", argv);
         }
-        // else
-        // {
-        //     while((wait(&status)) > 0);
-        //     pid3 = fork();
-        //     if(pid3 == 0)
-        //     {
-        //         while ((dp = readdir(dir)) != NULL)
-        //         {
-        //             if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
-        //             {
-        //                 char token[100] = " ";
-        //                 strcpy(token, dp->d_name);
-        //                 char pfolderp[100] = "/home/ezhie/modul2/petshop";
-
-
-        //             }
-        //         }
-        //     }
-        // }
+        else
+        {
+            ((wait(&status)) > 0);
+        }
+        listFilesRecursively(location);
+        
     }
     
     
