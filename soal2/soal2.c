4ade59e2c5458dddefdc482f6e93a46ee969d3e7
Crazy-Newbie <55473294+Crazy-Newbie@users.noreply.github.com>
2021-04-17 17:12:52 +0800

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+char location[] = "/home/ezhie/modul2/petshop";
+
 
 int main()
 {
-    pid_t pid1;
+    pid_t pid1,pid2;
 
     if(pid1 < 0){
-        exit(EXIT_FAILURE);
+       return EXIT_FAILURE;
     }
-
+    
+    pid1 = fork();
     if(pid1 == 0){
-        char *argv[] = {"unzip", "pets.zip", "-d", "/home/ezhie/praktikum_2/soal2", NULL};
-        execv("/usr/bin/unzip", argv);
+        char *argv[] = {"mkdir", "-p", "-d", location, NULL};
+        execv("/usr/bin/mkdir", argv);
+    }
+    else
+    {
+        while((wait(&status)) > 0);
+        pid2 = fork();
+        if (pid2 == 0)
+        {
+            char *argv[] = {"unzip", "pets.zip", "-d", location, NULL};
+            execv("/bin/unzip", argv);
+        }
+        
+        
     }
 
     return 0;
